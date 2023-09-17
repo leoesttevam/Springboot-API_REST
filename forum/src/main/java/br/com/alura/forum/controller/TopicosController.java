34c9b78b30b2b327bc2e7bf82ebdcd5a54452c87
leoesttevam <leonardoestevam17@gmail.com>
2023-09-16 22:26:04 -0300

    Paginacao e ordenacao de recursos

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -39,14 +42,15 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDTO> lista(String nomeCurso) {
+	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable pageable) {
+		
 		if (nomeCurso == null) {
-            List<Topico> topicos = repository.findAll();
-            return TopicoDTO.converter(topicos);
-     } else {
-         List<Topico> topicos = repository.findByCursoNome(nomeCurso);
-             return TopicoDTO.converter(topicos);
-     }
+            	Page<Topico> topicos = repository.findAll(pageable);
+            	return TopicoDTO.converter(topicos);
+			} else {
+				Page<Topico> topicos = repository.findByCursoNome(nomeCurso, pageable);
+				return TopicoDTO.converter(topicos);
+			}
 	}
 	
 	@PostMapping
