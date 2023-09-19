d6d215e2dd59ef310c4d43a1ecbc6529ae57de9c
leoesttevam <leonardoestevam17@gmail.com>
2023-09-19 19:46:39 -0300

    Documente a API com Swagger

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -55,7 +55,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override  //Configuração de recursos estaticos(js, css, imagens, etc)
 	public void configure(WebSecurity web) throws Exception {
-		
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**","/configuration/**", "/swagger-resources/**");
 	}
 	
 }
